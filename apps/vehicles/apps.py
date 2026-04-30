98a86e080b58a384e7e5d0bb58f5ff38f78ad7e5
Md. Joni Akter Labu <mdjoniakterlabu@gmail.com>
2026-05-01 02:47:43 +0600

    updated add config

--- apps/vehicles/apps.py
+++ apps/vehicles/apps.py
@@ -2,4 +2,6 @@ from django.apps import AppConfig
 
 
 class VehiclesConfig(AppConfig):
-    name = 'vehicles'
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.vehicles'
+    label = 'vehicles'
