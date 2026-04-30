a0edbec876fd06f21495997ace26937b76031faf
Md. Joni Akter Labu <mdjoniakterlabu@gmail.com>
2026-05-01 02:51:01 +0600

    configure vehicles app in main project

--- config/settings.py
+++ config/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 
     # Local apps
     'apps.accounts',
+    'apps.vehicles',
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('apps.accounts.urls')),
+    path('api/vehicles/', include('apps.vehicles.urls')),
 ]
 
 # Serve media files in development
