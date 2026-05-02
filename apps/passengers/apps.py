079948f8fbb3dfbd93eddc76febc970d8355a395
Md. Joni Akter Labu <mdjoniakterlabu@gmail.com>
2026-05-03 05:29:35 +0600

    updated app config

--- apps/passengers/apps.py
+++ apps/passengers/apps.py
@@ -2,4 +2,6 @@ from django.apps import AppConfig
 
 
 class PassengersConfig(AppConfig):
-    name = 'passengers'
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.passengers'
+    label = 'passengers'
