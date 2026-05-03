cc542542e7619123126699fcc286be45f37868bb
Md. Joni Akter Labu <mdjoniakterlabu@gmail.com>
2026-05-04 04:23:21 +0600

    updated facilities app config

--- apps/facilities/apps.py
+++ apps/facilities/apps.py
@@ -2,4 +2,6 @@ from django.apps import AppConfig
 
 
 class FacilitiesConfig(AppConfig):
-    name = 'facilities'
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.facilities'
+    label = 'facilities'
